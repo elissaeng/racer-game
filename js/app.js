297d2f546cfcdaf4538133fc15ac0611cd9a5740
elissaeng@gmail.com <elissaeng@Elissas-MacBook-Pro.local>
2021-04-21 18:59:39 -0700

    Pirate ship 1

--- js/app.js
+++ js/app.js
@@ -1,46 +1,34 @@
 $('#click').on('click', function () { 
-   
-    // function ifCompleted(distance) {
-    //     if (isComplete === false) {
-    //         isComplete = true
-    //     }   else {
-    //         place = 'second'
-    //     }
-    //     console.log('ifCompleted function has been executed')
-    // }
-
-    //get width of boxes
-    //const boxWidth = $('.box').width();
-    
-    // get width of 'ocean'/body?
-    //const bodyWidth = $(window).width() - boxWidth; 
-
-    // blueDistance = 500
-    // console.log
     
     let blueDistance = 50
-    let redDistance = 10
+    let redDistance = 50
+
+       //get width of boxes
+    const boxWidth = $('.box').width();
+    
+    //get width of 'ocean'/body?
+    const bodyWidth = $(window).width() - boxWidth; 
 
     $(window).on("keypress", function (event) {
         console.log(event.which)
         if (event.which === 97) {  
-            $('#blue').animate({left: blueDistance}, 200, function (){
-                blueDistance += 5;
+            $('#pirate1').animate({left: blueDistance}, 300, function (){
+                blueDistance += 30;
             }) 
         }   else if (event.which === 108) {
-              $('#red').animate({left: redDistance}, 200, function () {
-                redDistance += 5
+              $('#red').animate({left: redDistance}, 300, function () {
+                redDistance += 30
             })
         }
-        if (blueDistance > 500) {
+        if (blueDistance > bodyWidth) {
             console.log('Blue came in ' + place)
-            const blueResult = $(`<h2>Blue came in ${place}</h2>`) 
+            const blueResult = $(`<h2 class='result'>Blue came in ${place}</h2>`) 
             $('body').append(blueResult);
             $(window).off("keypress")
         } 
-        if (redDistance > 100) {
+        if (redDistance > bodyWidth) {
             console.log('Red came in ' + place)
-            const redResult = $(`<h2>Red came in ${place}</h2>`)
+            const redResult = $(`<h2 class='result'>Red came in ${place}</h2>`)
             $('body').append(redResult);
             $(window).off("keypress")
         }
@@ -50,20 +38,20 @@ $('#click').on('click', function () {
         
     })
     
-    let isComplete = false;
-    let place = 'first';
+        let place = 'first';
 
         $('#reset').on('click', function () {
         console.log('reset');
         $('#blue').animate({left: 0}, 500, function (){
             console.log('finished');
             
-         })
+        })
         $('#red').animate({left: 0}, 500, function (){
             console.log('finished');
             
             
         })
+        $('h2').remove()
     })
 })
 
